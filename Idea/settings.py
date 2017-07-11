6035122d9d6eb5b4eccab8a2f59c6884e0d1495a
yuzhipeng <yuzp1996@qq.com>
2017-07-11 21:31:00 +0800

    static文件查找

--- Idea/settings.py
+++ Idea/settings.py
@@ -120,15 +120,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATICFILES_FINDERS = (
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder"
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-)
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+STATIC_ROOT = os.path.join(os.path.dirname(__file__),'admina/').replace('\\','/')
