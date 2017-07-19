c8131da082233f1ca8ae73f29ad23bd4c13229e3
3chrisprosise <1337074512@qq.com>
2017-07-19 15:38:09 +0800

    添加各种后台页面

--- admina/reload.py
+++ admina/reload.py
@@ -1,5 +1,7 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render_to_response,render
+from admina import models
+from django.core.paginator import Paginator
 
 @csrf_exempt
 def index(req):
@@ -8,12 +10,20 @@ def index(req):
 @csrf_exempt
 def user_label(req):
     if req.method == "GET":
-        return render(req, 'second/Label_User.html')
+        currentpage = 1
+        userlabel = models.UserLabel.objects.all().order_by('Id')
+        page = Paginator(userlabel, 6)
+        userLabel = page.page(currentpage).object_list
+        return render_to_response('second/Label_User.html', {'UserLabel': userLabel})
     if req.method == "POST":
         pass
 
 def user_project(req):
     if req.method == "GET":
-        return render(req, 'second/Label-Project.html')
+        currentpage = 1
+        projectlabel = models.ProjectLabel.objects.all().order_by('Id')
+        page = Paginator(projectlabel, 6)
+        projectLabel = page.page(currentpage).object_list
+        return render_to_response('second/Label_Project.html', {'ProjectLabel ': projectLabel})
     if req.method == "POST":
         pass
