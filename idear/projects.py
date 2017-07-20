f8fd0b969bccf338f90536a19207f81f53bd05d3
yuzhipeng <yuzp1996@qq.com>
2017-07-20 08:40:00 +0800

    提交项目

--- idear/projects.py
+++ idear/projects.py
@@ -6,15 +6,19 @@ from django.shortcuts import render
 from admina.models import Project
 from django.shortcuts import HttpResponse,Http404,render_to_response,HttpResponseRedirect
 
+from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
 
 """
 项目
 """
+@csrf_exempt
 def projects(req):
 	if req.method == "GET":
-		return render_to_response('project/projects.html')
+		projects = Project.objects.all()
+		return render_to_response('project/projects.html',{'projects':projects}) 
 	else:
-		return render_to_response('project/projects.html')
-		# projects = Project.objects.all()
-		# return HttpResponse(projects)
+		# return render_to_response('project/projects.html')
+		projects = Project.objects.all()
+		return render_to_response('project/projects.html',{'projects':projects}) 
