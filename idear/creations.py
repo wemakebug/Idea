fc386341f4d812539acbf36ee564d55fade1eb80
yuzhipeng <yuzp1996@qq.com>
2017-07-21 16:38:36 +0800

    creation.py文件的修改

--- idear/creations.py
+++ idear/creations.py
@@ -8,12 +8,22 @@ from django.shortcuts import render,HttpResponse,Http404,render_to_response,Http
 # Create your views here.
 
 
-'''
-创意灵感一级界面
-'''
 def creations(req):
+	'''
+    创意灵感一级二级页面项目显示
+
+	'''
     if req.method == 'GET':
-        creations = Creation.objects.all()
-        return render_to_response('creations/index.html',{'creations':creations})
+    	sign = req.GET['sign']
+        if sign != "":
+	        Creation2ProjectLabelObj = Creation2ProjectLabel.objects.get( projectLabel = sign)
+	        creations = Creation2ProjectLabelObj.Creation2ProjectLabel_Creation_set.all()
+	    else:
+            creations = Creation.objects.all()
+            projectLabels = ProjectLabel.objects.all()
+        return render_to_response('creations/index.html',{'creations':creations,'projectLabels':projectLabels})
     else:
-        pass 
+    	id = req.POST['creationId']
+    	creation = Creation.objects.fitler(id = id)
+    	return render_to_response('creations/sec_creations.html',{'creation':creation,})
+
