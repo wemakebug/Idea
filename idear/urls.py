811918ed20130ccdca5b8c55a80f244ce1dcc7d5
yuzhipeng <yuzp1996@qq.com>
2017-07-21 14:52:53 +0800

    创意文件爱你

--- idear/urls.py
+++ idear/urls.py
@@ -33,5 +33,5 @@ urlpatterns = [
     url(r'creations$', creations.creations),
     url(r'projects$', projects.projects),
     # url(r'projects$', projects.projects),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
