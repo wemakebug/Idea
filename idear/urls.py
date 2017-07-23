16ca9de0b0b54c2da7aa03ada8e7c3f408b08156
Blacker <blacker@aliyun.com>
2017-07-23 14:30:52 +0800

    修改时间样式

--- idear/urls.py
+++ idear/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     url(r'creations$', creations.creations),
     url(r'forgetPassword$', views.forgetPassword),
     url(r'apply$', views.apply),
-    url(r'recruit$', views.projects),
+    url(r'recruit$', projects.projects),
     url(r'redetails$', views.redetails),
     url(r'projects$', projects.projects),
     url(r'star$', creations.star)
