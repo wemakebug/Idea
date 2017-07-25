427db8d034ae10f1352e72c64b0428c11ad89a5c
yuzhipeng <yuzp1996@qq.com>
2017-07-25 13:17:23 +0800

    creations的.py与js文件

--- idear/static/creation/js/js.js
+++ idear/static/creation/js/js.js
@@ -1,4 +1,3 @@
-
  $(function () {
             $(".block").slice(0, 12).show();
             $(".more").on('click', function (e) {
@@ -14,24 +13,21 @@
             });
         });
 
+
 $(function () {
 
-$.cookie("user",1)
+$.cookie("user",3)
 
 userId = $.cookie("user")
-
+		
 $(".follow").click(function(){
    Id = $(this).attr("creation")
-   alert(Id)
    $.post("attend",{userId:userId,attendType:"1",Id:Id},function(data){
     if(data == 1)
-    	alert("点赞成功");
+    	alert("关注成功")
     else
     	alert("操作失败")
-
-
 })
-
 
 })
 
