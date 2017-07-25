e3f3deb69b6d302ae5e8b287f4de8b67d984be43
yuzhipeng <yuzp1996@qq.com>
2017-07-25 13:19:22 +0800

    pull之后的再次上穿

--- idear/urls.py
+++ idear/urls.py
@@ -26,16 +26,18 @@ urlpatterns = [
     url(r'regist$', views.regist),
     url(r'team$', views.team),
     url(r'teamdetails$', views.teamdetails),
+    url(r'teamhelpapplication$', views.teamhelpapplication),
     url(r'creations$', creations.creations),
     url(r'forgetPassword$', views.forgetPassword),
     url(r'apply$', views.apply),
-    url(r'recruit$', views.projects),
+    url(r'recruit$', projects.projects),
     url(r'redetails$', views.redetails),
     url(r'projects$', projects.projects),
-                  # url(r'projects$', projects.projects),
 
     url(r'star$', creations.star),
     url(r'attend$', creations.attend),
+
+    url(r'test/?(\d+)$', views.test)
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
