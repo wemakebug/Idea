297ab6a19ab8986f7d13e53535d864d18d62c4c0
3chrisprosise <1337074512@qq.com>
2017-09-10 08:31:14 +0800

    忘记密码界面验证码，登陆，注册界面首页js补全

--- idear/views.py
+++ idear/views.py
@@ -54,29 +54,36 @@ def login(req):
         return render_to_response('idea/login.html')
     if req.method == "POST":
         result = {}
+        result['email'] = None
         result['status'] = None
         result['message'] = ''
         result['username'] = None
-        result['UUID'] = None
-        email = req.POST['email']
-        password = req.POST['password']
-        if models.User.objects.filter(Q(Email=email)):
-            user = models.User.objects.get(Email=email)
-            if user.PassWord == password:
-                user.Uuid = uuid.uuid1()
-                result['status'] = 1
-                result['username'] = user.UserName
-                req.session['uuid'] = user.Uuid
-                result['message'] = '登陆成功'
-                return HttpResponse(json.dumps(result))
-            elif user.PassWord != password:
+        result['uuid'] = None
+        try:
+            email = req.POST['email']
+            password = req.POST['password']
+        except:
+            result['status'] = 0
+            result['message'] = '获取信息失败'
+            return HttpResponse(json.dumps(result))
+        else:
+            if models.User.objects.filter(Q(Email=email)):
+                user = models.User.objects.get(Email=email)
+                if user.PassWord == password:
+                    user.Uuid = uuid.uuid1()
+                    result['status'] = 1
+                    result['username'] = user.UserName
+                    req.session['uuid'] = str(user.Uuid)
+                    result['message'] = '登陆成功'
+                    return HttpResponse(json.dumps(result))
+                elif user.PassWord != password:
+                    result['status'] = 0
+                    result['message'] = '用户名或密码错误'
+                    return HttpResponse(json.dumps(result))
+            else:
                 result['status'] = 0
                 result['message'] = '用户名或密码错误'
                 return HttpResponse(json.dumps(result))
-        else:
-            result['status'] = 0
-            result['message'] = '用户名或密码错误'
-            return HttpResponse(json.dumps(result))
 
 
 
@@ -117,39 +124,44 @@ def regist(req):
             'message': None,
             'status': 0,
             'username': None,
-            'account': None,
+            'emial': None,
             'uuid': None
         }
-        username = req.POST['UserName']
-        email = req.POST['Email']
-        password = req.POST['Passwd']
-        print('well')
-        if not (varidate_char(username) and varidate_emial(email)):
-            result['message'] = '输入非法字符'
+        try:
+            username = req.POST['UserName']
+            email = req.POST['Email']
+            password = req.POST['Passwd']
+        except:
             result['status'] = 0
-            return HttpResponse(json.dumps(result))
-        elif models.User.objects.filter(Email=email):
-            result['status'] = 0
-            result['message'] = '邮箱已经被注册'
-            return HttpResponse(json.dumps(result))
-        elif models.User.objects.filter(UserName=username):
-            result['status'] = 0
-            result['message'] = '姓名已被注册'
+            result['message'] = '获取信息失败'
             return HttpResponse(json.dumps(result))
         else:
-            try:
-                models.User.objects.create(Email=email, UserName=username, PassWord=password, Uuid=uuid.uuid1())
-                user = models.User.objects.get(Email=email)
-                req.session['uuid'] = user.Uuid
-                result['username'] = username
-                result['account'] = email
-                result['message'] = '注册成功，正在调转'
-                result['status'] = 1
-                return HttpResponse(json.dumps(result))
-            except:
+            if not (varidate_char(username) and varidate_emial(email)):
+                result['message'] = '输入非法字符'
                 result['status'] = 0
-                result['message'] = '服务器异常'
                 return HttpResponse(json.dumps(result))
+            elif models.User.objects.filter(Email=email):
+                result['status'] = 0
+                result['message'] = '邮箱已经被注册'
+                return HttpResponse(json.dumps(result))
+            elif models.User.objects.filter(UserName=username):
+                result['status'] = 0
+                result['message'] = '姓名已被注册'
+                return HttpResponse(json.dumps(result))
+            else:
+                try:
+                    models.User.objects.create(Email=email, UserName=username, PassWord=password, Uuid=uuid.uuid1())
+                    user = models.User.objects.get(Email=email)
+                    req.session['uuid'] = str(user.Uuid)
+                    result['email'] = email
+                    result['username'] = username
+                    result['message'] = '注册成功，正在调转'
+                    result['status'] = 1
+                    return HttpResponse(json.dumps(result))
+                except Exception,e:
+                    result['status'] = 0
+                    result['message'] = '服务器异常!!' + e
+                    return HttpResponse(json.dumps(result))
 
 
 def team(req):
