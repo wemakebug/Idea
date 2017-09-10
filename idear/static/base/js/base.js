1e4d9e1f5659fc3558dd2ee386bf735a9b7925a6
张凌雪 <1050080567@qq.com>
2017-09-10 17:41:06 +0800

    更新

--- idear/static/base/js/base.js
+++ idear/static/base/js/base.js
@@ -2,3 +2,12 @@
  * Created by root on 17-9-8.
  */
 alert(window.cookie);
+
+$(document).ready(function(){
+    $(".nav li a").each(function(){
+        $this = $(this);
+        if($(this)[0].href==String(window.location)) {
+            $(this).addClass('active');
+        }
+    });
+});
