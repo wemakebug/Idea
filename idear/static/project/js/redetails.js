cf9ba5b8f6d6b9193f8a09a1b9a66c2d6122573a
wangyangyangsky <1028658627@qq.com>
2017-10-29 21:29:06 +0800

    招募帖进度条

--- idear/static/project/js/redetails.js
+++ idear/static/project/js/redetails.js
@@ -1,21 +1,8 @@
-          // 招募进度条
-            $(function () {
-                //var $ppc = $('.progress-pie-chart').val(),
-                    //percent = parseInt($ppc.data('percent')),
 
-              //  var percent = {{ recruit.RecruitedNumber }}/{{ recruit.PredictNumber }};
-                    deg = 360 * percent ;
-                if (percent > 50) {
-                    $ppc.addClass('gt-50');
-                }
-                if (percent > 100){
-                    deg = 360;
-                }
-                $('.ppc-progress-fill').css('transform', 'rotate(' + deg + 'deg)');
-                  //$('.ppc-percents span').html( percent + '/' +  );
+       //$('.ppc-percents span').html( percent + '/' +  );
               // $('.ppc-percents span').html('1' + '/' + percent/100);
                 //进度条中间显示计算 已招募人数/预计招募人数
-                });
+
                 $("#comment11-22").click(function () {
                    document.getElementById("comment-content1").focus();
                 });
